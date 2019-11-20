0d3a1cbe63f8fb447ad8a9cad18708d7b40a6a2b
Dmitry_Neustupov <neustupov@yandex.ru>
2019-11-20 07:51:38 +0300

    table_per_class strategy

--- src/main/java/ru/neustupov/model/inheritance/mappedsuperclass/BillingDetails.java
+++ src/main/java/ru/neustupov/model/inheritance/mappedsuperclass/BillingDetails.java
@@ -1,10 +1,19 @@
 package ru.neustupov.model.inheritance.mappedsuperclass;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class BillingDetails {
+
+  @Id
+  @GeneratedValue(generator = "ID_GENERATOR")
+  private Long id;
 
   @NotNull
   private String owner;
--- src/main/java/ru/neustupov/model/inheritance/mappedsuperclass/CreditCard.java
+++ src/main/java/ru/neustupov/model/inheritance/mappedsuperclass/CreditCard.java
@@ -1,22 +1,10 @@
 package ru.neustupov.model.inheritance.mappedsuperclass;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@AttributeOverride(
-    name = "owner",
-    column = @Column(name = "CC_OWNER", nullable = false)
-)
 public class CreditCard extends BillingDetails {
-
-  @Id
-  @GeneratedValue(generator = "ID_GENERATOR")
-  private Long id;
 
   @NotNull
   private String cardNumber;
